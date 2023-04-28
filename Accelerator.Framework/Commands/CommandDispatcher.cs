098a1ef8193124017b3abc88437721408f07e8c2
ser <se_rahbar@outlook.com>
2023-04-28 21:16:37 +0330

    CommandDispatcher class dynamicly choosing commandHandler of a command

---
+++ Accelerator.Framework/Commands/CommandDispatcher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accelerator.Framework.Commands
+{
+    public class CommandDispatcher
+    {
+        private readonly IServiceProvider _provider;
+
+        public CommandDispatcher(IServiceProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public CommandResult Dispatch(ICommand command)
+        {
+            Type type = typeof(CommandHandler<>);
+            Type[] typeArgs = { command.GetType() };
+            Type handlerType = type.MakeGenericType(typeArgs);
+            dynamic handler = _provider.GetService(handlerType);
+            CommandResult result = handler.Handle((dynamic)command);
+            return result;
+        }
+    }
+}
