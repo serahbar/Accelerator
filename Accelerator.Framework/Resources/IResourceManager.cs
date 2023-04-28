eb9bda5213a688edfafce71bc673bafc71764204
ser <se_rahbar@outlook.com>
2023-04-28 21:03:55 +0330

    IResourceManager interface

---
+++ Accelerator.Framework/Resources/IResourceManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accelerator.Framework.Resources
+{
+    public interface IResourceManager
+    {
+        string GetName(string name);
+        string GetName(string name, params string[] arguments);
+        string this[string name] { get; }
+        string this[string name, params string[] arguments] { get; }
+    }
+}
