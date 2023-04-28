f8ef243cd73e9ea827dcbad715a744159482fab8
ser <se_rahbar@outlook.com>
2023-04-28 20:10:01 +0330

    Next step will be: generating Guid depond on Time for sorting

--- Accelerator.Framework/Domain/BaseEntity.cs
+++ Accelerator.Framework/Domain/BaseEntity.cs
@@ -8,6 +8,7 @@ namespace Accelerator.Framework.Domain
 {
     public class BaseEntity
     {
+        //Next step will be: generating Guid depond on Time for sorting
         public Guid Id { get; set; }
     }
 }
