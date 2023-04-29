daf6dedd069beb3c17124670b016a6af325b5484
ser <se_rahbar@outlook.com>
2023-04-29 19:44:12 +0330

    SharedResource class

---
+++ Accelerator.Core.Resources/Resources/SharedResource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accelerator.Core.Resources.Resources
+{
+    public class SharedResource
+    {
+        public const string AppName = nameof(AppName);
+    }
+}
