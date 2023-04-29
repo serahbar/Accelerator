d0a52e1fe8b289a9a46351314356bc9f81e4958b
ser <se_rahbar@outlook.com>
2023-04-29 20:05:53 +0330

    add Author and Course entity

---
+++ Accelerator.Core.Domain/Autors/Entities/Author.cs
@@ -0,0 +1,31 @@
+﻿using Accelerator.Core.Domain.Courses.Entities;
+
+namespace Accelerator.Core.Domain.Autors.Entities
+{
+    public class Author
+    {
+
+        public Guid Id { get; set; }
+
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTimeOffset DateOfBirth { get; set; }
+
+        public DateTimeOffset? DateOfDeath { get; set; }
+
+        public string MainCategory { get; set; }
+
+        public ICollection<Course> Courses { get; set; }
+            = new List<Course>();
+
+        public Author(string firstName, string lastName, string mainCategory)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            MainCategory = mainCategory;
+        }
+    }
+}
---
+++ Accelerator.Core.Domain/Courses/Entities/Course.cs
@@ -0,0 +1,28 @@
+﻿using Accelerator.Core.Domain.Autors.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accelerator.Core.Domain.Courses.Entities
+{
+    public class Course
+    {
+
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public Author Author { get; set; } = null!;
+
+        public Guid AuthorId { get; set; }
+
+        public Course(string title)
+        {
+            Title = title;
+        }
+    }
+}
