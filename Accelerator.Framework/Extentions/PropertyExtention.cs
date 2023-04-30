268085130bd79d304f8eed507c7bbfdd30c317bf
ser <se_rahbar@outlook.com>
2023-04-30 23:48:28 +0330

    Create Extentions folder PropertyExtention class to list properties of a class

---
+++ Accelerator.Framework/Extentions/PropertyExtention.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+namespace Accelerator.Framework.Extentions
+{
+    public static class PropertyExtention<T> where T : class
+    {
+        public static List<string> GetClassProperty()
+        {
+            List<string> result = new List<string>();
+            PropertyInfo[] propertyInfos;
+            propertyInfos = typeof(T).GetProperties();
+            Array.Sort(propertyInfos,
+                delegate (PropertyInfo propertyInfo1, PropertyInfo propertyInfo2)
+                { 
+                    return propertyInfo1.Name.CompareTo(propertyInfo2.Name); 
+                });
+            foreach (PropertyInfo propertyInfo in propertyInfos)
+            {
+                result.Add(propertyInfo.Name);
+            }
+
+            return result;
+        }
+    }
+}
