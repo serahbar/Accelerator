5a9f59851c61a164169b7090eee085fea1e6fd8a
ser <se_rahbar@outlook.com>
2023-05-01 22:42:41 +0330

    DateTimeOffsetExtensions

---
+++ Accelerator.Framework/Extentions/DateTimeOffsetExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accelerator.Framework.Extentions
+{
+    public static class DateTimeOffsetExtensions
+    {
+        public static int GetCurrentAge(this DateTimeOffset dateTimeOffset,
+            DateTimeOffset? dateOfDeath)
+        {
+            var dateToCalculateTo = DateTime.UtcNow;
+
+            if (dateOfDeath != null)
+            {
+                dateToCalculateTo = dateOfDeath.Value.UtcDateTime;
+            }
+
+            var age = dateToCalculateTo.Year - dateTimeOffset.Year;
+
+            if (dateToCalculateTo < dateTimeOffset.AddYears(age))
+            {
+                age--;
+            }
+
+            return age;
+        }
+
+    }
+}
