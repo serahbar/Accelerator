327f1b349931b4f0e39ecce01373df1b2143cfe1
ser <se_rahbar@outlook.com>
2023-05-01 22:42:20 +0330

    AuthorsProfile need by automapper

---
+++ Accelerator.Core.Domain/Autors/Profiles/AuthorsProfile.cs
@@ -0,0 +1,29 @@
+﻿using Accelerator.Core.Domain.Autors.Dtoes;
+using Accelerator.Core.Domain.Autors.Entities;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accelerator.Core.Domain.Autors.Profiles
+{
+    public class AuthorsProfile : Profile
+    {
+        public AuthorsProfile()
+        {
+            CreateMap<Author,AuthorDto>()
+                .ForMember(dest => dest.Name, opt =>
+                    opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
+                .ForMember(dest => dest.Age, opt =>
+                    opt.MapFrom(src => src.DateOfBirth.GetCurrentAge(src.DateOfDeath)));
+
+            CreateMap<AuthorForCreationDto, Entities.Author>();
+
+            CreateMap<Author,AuthorFullDto>();
+
+            CreateMap<AuthorForCreationWithDateOfDeathDto,Author>();
+        }
+    }
+}
