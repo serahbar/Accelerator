f4f86783ed8e3997ccc2f4fbdeeea176a51f03ff
ser <se_rahbar@outlook.com>
2023-05-01 22:05:32 +0330

    DictionaryExtention class

---
+++ Accelerator.Framework/Extentions/DictionaryExtention.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Accelerator.Framework.Extentions
+{
+    public static class DictionaryExtention
+    {
+        public static Dictionary<string,string> StringToDectionary(string str)
+        {
+            Dictionary<string,string> result=new Dictionary<string, string>();
+            result=JsonConvert.DeserializeObject<Dictionary<string,string>>(str);
+
+            return result;
+        }
+    }
+}
