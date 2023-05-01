263145378e4a98ab0a3bc72ca6cb96fa74d99d6f
ser <se_rahbar@outlook.com>
2023-05-01 22:25:05 +0330

    AuthorForCreationWithDateOfDeathDto

---
+++ Accelerator.Core.Domain/Autors/Dtoes/AuthorForCreationWithDateOfDeathDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accelerator.Core.Domain.Autors.Dtoes
+{
+    public class AuthorForCreationWithDateOfDeathDto : AuthorForCreationDto
+    {
+        public DateTimeOffset? DateOfDeath { get; set; }
+    }
+}
