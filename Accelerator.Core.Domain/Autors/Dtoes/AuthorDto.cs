ce8cb84f96673e4d1fc25a4f88a63595edfbe670
ser <se_rahbar@outlook.com>
2023-05-01 22:22:01 +0330

    AuthorDto class

---
+++ Accelerator.Core.Domain/Autors/Dtoes/AuthorDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accelerator.Core.Domain.Autors.Dtoes
+{
+    public class AuthorDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public string MainCategory { get; set; } = string.Empty;
+
+    }
+}
