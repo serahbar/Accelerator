7e8eb0a1d79cab0c03d5f084a28bddacb1ade15a
ser <se_rahbar@outlook.com>
2023-05-01 22:25:57 +0330

    AuthorFullDto

---
+++ Accelerator.Core.Domain/Autors/Dtoes/AuthorFullDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accelerator.Core.Domain.Autors.Dtoes
+{
+    public class AuthorFullDto
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTimeOffset DateOfBirth { get; set; }
+        public string MainCategory { get; set; } = string.Empty;
+    }
+}
