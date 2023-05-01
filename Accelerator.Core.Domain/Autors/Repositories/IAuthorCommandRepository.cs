d7fc5bae7fbb95700f379d8b70e4a013e6d87372
ser <se_rahbar@outlook.com>
2023-05-01 22:22:28 +0330

    Author Repository interface

---
+++ Accelerator.Core.Domain/Autors/Repositories/IAuthorCommandRepository.cs
@@ -0,0 +1,14 @@
+﻿using Accelerator.Core.Domain.Autors.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accelerator.Core.Domain.Autors.Repositories
+{
+    public interface IAuthorCommandRepository
+    {
+        void Add(Author author);
+    }
+}
---
+++ Accelerator.Core.Domain/Autors/Repositories/IAuthorQueryRepository.cs
@@ -0,0 +1,10 @@
+﻿using Accelerator.Core.Domain.Autors.Entities;
+
+namespace Accelerator.Core.Domain.Autors.Repositories
+{
+    public interface IAuthorQueryRepository
+    {
+        Author Get(Guid id);
+        List<Author> GetAll();
+    }
+}
