27e35f4e2025a593d8f986620a7760d6fb35e712
ser <se_rahbar@outlook.com>
2023-05-03 18:01:06 +0330

    edit AddAuthorCommand

--- Accelerator.Core.Domain/Autors/Commands/AddAuthorCommand.cs
+++ Accelerator.Core.Domain/Autors/Commands/AddAuthorCommand.cs
@@ -1,25 +1,12 @@
-﻿using Accelerator.Core.Domain.Autors.Entities;
+﻿using Accelerator.Core.Domain.Autors.Dtoes;
+using Accelerator.Core.Domain.Autors.Entities;
 using Accelerator.Core.Domain.Courses.Entities;
 using Accelerator.Framework.Commands;
 
 namespace Accelerator.Core.Domain.Autors.Commands
 {
-    public class AddAuthorCommand : ICommand
+    public class AddAuthorCommand :AuthorForCreationDto, ICommand
     {
 
-        public string FirstName { get; set; }
-
-        public string LastName { get; set; }
-
-        public AuthorType AuthorType { get; set; }
-
-        public DateTimeOffset DateOfBirth { get; set; }
-
-        public DateTimeOffset? DateOfDeath { get; set; }
-
-        public string MainCategory { get; set; }
-
-        public ICollection<Course>? Courses { get; set; }
-            = new List<Course>();
     }
 }
