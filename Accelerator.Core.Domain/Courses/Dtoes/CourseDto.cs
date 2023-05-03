a69310a87b22e8815131bb895e3830df03b7e811
ser <se_rahbar@outlook.com>
2023-05-03 18:04:42 +0330

    Course Dto class

---
+++ Accelerator.Core.Domain/Courses/Dtoes/CourseDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accelerator.Core.Domain.Courses.Dtoes
+{
+    public class CourseDto
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+    }
+}
