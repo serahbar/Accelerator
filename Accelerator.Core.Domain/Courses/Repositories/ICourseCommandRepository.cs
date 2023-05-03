e6a9649bffeba5c85a15ddd9cac5c5c0a8fafdec
ser <se_rahbar@outlook.com>
2023-05-03 18:13:21 +0330

    course Repository Interface

---
+++ Accelerator.Core.Domain/Courses/Repositories/ICourseCommandRepository.cs
@@ -0,0 +1,14 @@
+﻿using Accelerator.Core.Domain.Courses.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accelerator.Core.Domain.Courses.Repositories
+{
+    public interface ICourseCommandRepository
+    {
+        void Add(Course course);
+    }
+}
---
+++ Accelerator.Core.Domain/Courses/Repositories/ICourseQueryRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Accelerator.Core.Domain.Courses.Repositories
+{
+    public interface ICourseQueryRepository
+    {
+    }
+}
