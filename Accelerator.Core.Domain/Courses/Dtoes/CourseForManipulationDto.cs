d50803829a4ce42607d6f530025141f3968f033e
ser <se_rahbar@outlook.com>
2023-05-03 18:10:25 +0330

    Dto class for Course

---
+++ Accelerator.Core.Domain/Courses/Dtoes/CourseForManipulationDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accelerator.Core.Domain.Courses.Dtoes
+{
+
+    [CourseTitleMustBeDifferentFromDescription]
+    public abstract class CourseForManipulationDto //: IValidatableObject
+    {
+        [Required(ErrorMessage = "You should fill out a title.")]
+        [MaxLength(100, ErrorMessage = "The title shouldn't have more than 100 characters.")]
+        public string Title { get; set; } = string.Empty;
+
+        [MaxLength(1500, ErrorMessage = "The description shouldn't have more than 1500 characters.")]
+        public virtual string Description { get; set; } = string.Empty;
+
+        //public IEnumerable<ValidationResult> Validate(
+        //    ValidationContext validationContext)
+        //{
+        //    if (Title == Description)
+        //    {
+        //        yield return new ValidationResult(
+        //        "The provided description should be different from the title.",
+        //        new[] { "Course" });
+        //    }
+        //}
+    }
+}
---
+++ Accelerator.Core.Domain/Courses/Dtoes/CourseForUpdateDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accelerator.Core.Domain.Courses.Dtoes
+{
+    public class CourseForUpdateDto : CourseForManipulationDto
+    {
+        [Required(ErrorMessage = "You should fill out a description.")]
+        public override string Description
+        {
+            get => base.Description; set => base.Description = value;
+        }
+    }
+}
