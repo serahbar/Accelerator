c22e5c3bc5c1d3f74dfeda390e0ad44cf4e57345
ser <se_rahbar@outlook.com>
2023-05-03 18:18:38 +0330

    Course title must be different from description

---
+++ Accelerator.Core.Domain/Courses/ValidationAttributes/CourseTitleMustBeDifferentFromDescriptionAttribute.cs
@@ -0,0 +1,41 @@
+﻿using Accelerator.Core.Domain.Courses.Dtoes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accelerator.Core.Domain.Courses.ValidationAttributes
+{
+    public class CourseTitleMustBeDifferentFromDescriptionAttribute
+       : ValidationAttribute
+    {
+        public CourseTitleMustBeDifferentFromDescriptionAttribute()
+        {
+        }
+
+        protected override ValidationResult? IsValid(object? value,
+            ValidationContext validationContext)
+        {
+            if (validationContext.ObjectInstance is not
+                CourseForManipulationDto course)
+            {
+                throw new Exception($"Attribute " +
+                    $"{nameof(CourseTitleMustBeDifferentFromDescriptionAttribute)} " +
+                    $"must be applied to a " +
+                    $"{nameof(CourseForManipulationDto)} or derived type.");
+            }
+
+            if (course.Title == course.Description)
+            {
+                return new ValidationResult(
+                "The provided description should be different from the title.",
+                    new[] { nameof(CourseForManipulationDto) });
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+
+}
