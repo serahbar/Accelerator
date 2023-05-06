2cf4f6f41a02c4d1e8e28aa146bf0b5e3f0a705b
ser <se_rahbar@outlook.com>
2023-05-06 23:23:27 +0330

    Add command and query Repository

---
+++ Accelerator.Infrastructures.Data.SqlServer/Courses/Repositories/CourseCommandRepository.cs
@@ -0,0 +1,18 @@
+﻿using Accelerator.Core.Domain.Courses.Entities;
+using Accelerator.Core.Domain.Courses.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accelerator.Infrastructures.Data.SqlServer.Courses.Repositories
+{
+    public class CourseCommandRepository : ICourseCommandRepository
+    {
+        public void Add(Course course)
+        {
+            throw new NotImplementedException();
+        }
+    }  
+}
---
+++ Accelerator.Infrastructures.Data.SqlServer/Courses/Repositories/CourseQueryRepository.cs
@@ -0,0 +1,9 @@
+﻿using Accelerator.Core.Domain.Courses.Entities;
+using Accelerator.Core.Domain.Courses.Repositories;
+
+namespace Accelerator.Infrastructures.Data.SqlServer.Courses.Repositories
+{
+    public class CourseQueryRepository : ICourseQueryRepository
+    {
+    }
+}
