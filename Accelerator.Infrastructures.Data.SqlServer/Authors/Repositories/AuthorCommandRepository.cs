cd905884a25a395fa533d56d44a55f9001a069f5
ser <se_rahbar@outlook.com>
2023-05-06 23:16:59 +0330

    add AuthorCommandRepository class

---
+++ Accelerator.Infrastructures.Data.SqlServer/Authors/Repositories/AuthorCommandRepository.cs
@@ -0,0 +1,18 @@
+﻿using Accelerator.Core.Domain.Authors.Entities;
+using Accelerator.Core.Domain.Authors.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accelerator.Infrastructures.Data.SqlServer.Authors.Repositories
+{
+    public class AuthorCommandRepository : IAuthorCommandRepository
+    {
+        public void Add(Author author)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
