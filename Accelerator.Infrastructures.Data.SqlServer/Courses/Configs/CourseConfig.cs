e698480a43e8d57e2aa0683ee9d9dd82edc36ccf
ser <se_rahbar@outlook.com>
2023-05-06 23:15:11 +0330

    Add CourseConfig class to config course

---
+++ Accelerator.Infrastructures.Data.SqlServer/Courses/Configs/CourseConfig.cs
@@ -0,0 +1,24 @@
+﻿using Accelerator.Core.Domain.Courses.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accelerator.Infrastructures.Data.SqlServer.Courses.Configs
+{
+    public class CourseConfig : IEntityTypeConfiguration<Course>
+    {
+        public void Configure(EntityTypeBuilder<Course> entity)
+        {
+           entity.HasKey(x=>x.Id);
+            entity.Property(x => x.Title).IsRequired();
+            entity.Property<DateTime>("CreateTime");
+            entity.Property<Guid>("CreateBy");
+        }
+
+    }
+    
+}
