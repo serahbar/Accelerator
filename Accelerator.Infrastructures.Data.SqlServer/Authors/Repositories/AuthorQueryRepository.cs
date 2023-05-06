2bcb3d139603686bbd25672a24498d370a7e74e6
ser <se_rahbar@outlook.com>
2023-05-06 23:20:50 +0330

    add AuthorQueryRepository class

---
+++ Accelerator.Infrastructures.Data.SqlServer/Authors/Repositories/AuthorQueryRepository.cs
@@ -0,0 +1,23 @@
+﻿using Accelerator.Core.Domain.Authors.Entities;
+using Accelerator.Core.Domain.Authors.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accelerator.Infrastructures.Data.SqlServer.Authors.Repositories
+{
+    public class AuthorQueryRepository : IAuthorQueryRepository
+    {
+        public Author Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Author> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
