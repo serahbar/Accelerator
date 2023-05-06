10f43d4a859aa40afe4ec5fba91f832729e02f77
ser <se_rahbar@outlook.com>
2023-05-06 23:52:39 +0330

    AddAuthorCommandHandler class

---
+++ Accelerator.Core.ApplicationServices/Authors/Commands/AddAuthorCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using Accelerator.Core.Domain.Authors.Commands;
+using Accelerator.Core.Domain.Authors.Repositories;
+using Accelerator.Framework.Commands;
+using Accelerator.Framework.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accelerator.Core.ApplicationServices.Authors.Commands
+{
+    public class AddAuthorCommandHandler : CommandHandler<AddAuthorCommand>
+    {
+        private readonly IAuthorCommandRepository _commandRepository;   
+        public AddAuthorCommandHandler(IResourceManager resourceManager,IAuthorCommandRepository commandRepository): base(resourceManager)
+        {
+            _commandRepository = commandRepository;
+        }
+        public override CommandResult Handle(AddAuthorCommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
