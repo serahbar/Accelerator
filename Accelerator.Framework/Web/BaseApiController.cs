fa5bb1a19178bda2314a60746b511343aeae65be
ser <se_rahbar@outlook.com>
2023-05-19 13:02:44 +0330

    BaseApiController

---
+++ Accelerator.Framework/Web/BaseApiController.cs
@@ -0,0 +1,26 @@
+﻿using Accelerator.Framework.Commands;
+using Accelerator.Framework.Queries;
+using Accelerator.Framework.Resources;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Accelerator.Framework.Web
+{
+    [ApiController]
+    public class BaseApiController: ControllerBase
+    {
+        protected readonly CommandDispatcher _commandDispatcher;
+        protected readonly QueryDispatcher _queryDispatcher;
+        protected readonly IResourceManager _resourceManager;
+
+        public BaseApiController(CommandDispatcher commandDispatcher, 
+                                    QueryDispatcher queryDispatcher,
+                                        IResourceManager resourceManager)
+        {
+            _commandDispatcher = commandDispatcher;
+            _queryDispatcher = queryDispatcher;
+            _resourceManager = resourceManager;
+        }
+
+  
+    }
+}
