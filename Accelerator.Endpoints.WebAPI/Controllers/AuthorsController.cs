6f371d7cd6aff467301a888d97cf00f8d432d96c
ser <se_rahbar@outlook.com>
2023-05-20 19:55:43 +0330

    AuthorController class

---
+++ Accelerator.Endpoints.WebAPI/Controllers/AuthorsController.cs
@@ -0,0 +1,25 @@
+﻿using Accelerator.Framework.Commands;
+using Accelerator.Framework.Queries;
+using Accelerator.Framework.Resources;
+using Accelerator.Framework.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Accelerator.Endpoints.WebAPI.Controllers
+{
+    public class AuthorsController : BaseApiController
+    {
+      
+
+        public AuthorsController(CommandDispatcher commandDispatcher,
+                QueryDispatcher queryDispatcher,
+                IResourceManager resourceManager) : base(commandDispatcher, queryDispatcher, resourceManager)
+        {
+        }
+        public async Task<IActionResult> GetAuthors()
+        {
+            //TODO:Test
+            return null;
+        }
+        
+    }
+}
