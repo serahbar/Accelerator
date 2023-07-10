c7f18053b1527b635f791d59b67ca66662a52e97
ser <se_rahbar@outlook.com>
2023-07-10 22:51:10 +0330

    قابلیت سرج و صفحه بندی

--- Accelerator.Endpoints.WebAPI/Controllers/AuthorsController.cs
+++ Accelerator.Endpoints.WebAPI/Controllers/AuthorsController.cs
@@ -1,4 +1,5 @@
-﻿using Accelerator.Framework.Commands;
+﻿using Accelerator.Core.Domain.Authors.Dtoes;
+using Accelerator.Framework.Commands;
 using Accelerator.Framework.Queries;
 using Accelerator.Framework.Resources;
 using Accelerator.Framework.Web;
@@ -6,20 +7,65 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Accelerator.Endpoints.WebAPI.Controllers
 {
+    [ApiController]
+    [Route("api/authors")]
     public class AuthorsController : BaseApiController
     {
-      
+
 
         public AuthorsController(CommandDispatcher commandDispatcher,
                 QueryDispatcher queryDispatcher,
                 IResourceManager resourceManager) : base(commandDispatcher, queryDispatcher, resourceManager)
         {
         }
-        public async Task<IActionResult> GetAuthors()
+        [HttpGet(Name = "GetAuthors")]
+        public async Task<IActionResult> GetAuthors(
+            [FromQuery] AuthorsResourceParameters authorsResourceParameters)
         {
             //TODO:Test
-            return null;
+            return Ok();
         }
-        
+        [HttpGet("{authorId}")]
+        public async Task<IActionResult> GetFullAuthorWithLinks(Guid authorId,
+    string? fields)
+        {
+            return Ok();
+        }
+        [HttpGet("{authorId}", Name = "GetAuthor")]
+        public async Task<IActionResult> GetFullAuthorWithoutLinks(Guid authorId,
+    string? fields)
+        {
+            return Ok();
+        }
+        [HttpGet("{authorId}")]
+        public async Task<IActionResult> GetAuthorWithLinks(Guid authorId,
+    string? fields)
+        {
+            return Ok();
+        }
+        [HttpGet("{authorId}", Name = "GetAuthor")]
+        public async Task<IActionResult> GetAuthorWithoutLinks(Guid authorId,
+    string? fields)
+        {
+            return Ok();
+        }
+        [HttpPost(Name = "CreateAuthorWithDateOfDeath")]
+        public async Task<IActionResult> CreateAuthorWithDateOfDeath(AuthorForCreationWithDateOfDeathDto author)
+        {
+            return Ok();
+        }
+        [HttpPost(Name = "CreateAuthor")]
+        public async Task<ActionResult<AuthorDto>> CreateAuthor(
+     AuthorForCreationDto author)
+        {
+            return Ok();
+        }
+        [HttpOptions()]
+        public IActionResult GetAuthorsOptions()
+        {
+            Response.Headers.Add("Allow", "GET,HEAD,POST,OPTIONS");
+            return Ok();
+        }
+
     }
 }
---
+++ Accelerator.Endpoints.WebAPI/ResourceParameters/AuthorsResourceParameters.cs
@@ -0,0 +1,19 @@
+﻿namespace Accelerator.Endpoints.WebAPI.ResourceParameters
+{
+    public class AuthorsResourceParameters
+    {
+        const int maxPageSize = 50;
+        public string? MainCategory { get; set; }
+        public string? SearchQuery { get; set; }
+        public int PageNumber { get; set; } = 1;
+        private int _pageSize=10;
+
+        public int PageSize
+        {
+            get=>_pageSize; 
+            set => _pageSize = (value>maxPageSize)?maxPageSize:value;
+        }
+        public string OrderBy { get; set; } = "Name";
+        public string? Fields { get; set; }
+    }
+}
