dafed595b60c915e7cf0f46c71a70f2d3d93a94d
ser <se_rahbar@outlook.com>
2023-07-23 19:40:36 +0330

    AuthorResourceParameters

---
+++ Accelerator.Core.Domain/Authors/ResourceParameters/AuthorsResourceParameters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accelerator.Core.Domain.Authors.ResourceParameters
+{
+    public class AuthorsResourceParameters
+    {
+        const int maxPageSize = 50;
+        public string? MainCategory { get; set; }
+        public string? SearchQuery { get; set; }
+        public int PageNumber { get; set; } = 1;
+        private int _pageSize = 10;
+
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = (value > maxPageSize) ? maxPageSize : value;
+        }
+        public string OrderBy { get; set; } = "Name";
+        public string? Fields { get; set; }
+    }
+}
